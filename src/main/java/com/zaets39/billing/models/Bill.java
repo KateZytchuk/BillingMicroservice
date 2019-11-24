5f117fb94ceb644256de1ccf90ec0a4f074dacde
zaets39 <katezytchuk24@gmail.com>
2019-11-24 20:55:55 +0200

    Model class added. There some doubts about 2 fields(startAddress, finishAddress). Lombok annotation @Data added. Constructor to work with json added.

---
+++ src/main/java/com/zaets39/billing/models/Bill.java
@@ -0,0 +1,45 @@
+package com.zaets39.billing.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class Bill {
+    private double amount;
+    private boolean babySeat;
+    private boolean englishDriver;
+    private boolean conditioner;
+    private boolean pet;
+    private boolean courier;
+    private boolean nonSmoker;
+    private boolean silence;
+    private String carType;
+    private double distance;
+    private String paymentMode;
+
+    //I`m not sure, about my need for these fields
+    // private String startAddress;
+    // private String destinationAddress;
+
+    public Bill(@JsonProperty("babySeat") boolean babySeat,
+                @JsonProperty("english") boolean englishDriver,
+                @JsonProperty("conditioner") boolean conditioner,
+                @JsonProperty("pet") boolean pet,
+                @JsonProperty("courier") boolean courier,
+                @JsonProperty("nonsmoker") boolean nonSmoker,
+                @JsonProperty("conditioner") boolean silence,
+                @JsonProperty("type") String carType,
+                @JsonProperty("distance") double distance,
+                @JsonProperty("paymentMode") String paymentMode) {
+        this.babySeat = babySeat;
+        this.englishDriver = englishDriver;
+        this.conditioner=conditioner;
+        this.pet = pet;
+        this.courier = courier;
+        this.nonSmoker = nonSmoker;
+        this.silence = silence;
+        this.carType = carType;
+        this.distance = distance;
+        this.paymentMode = paymentMode;
+    }
+}
